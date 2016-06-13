5745ed7e391f677c6fb587e7d26204452fde1299
wellsonjain <wellsonwei@ecoworkinc.com>
2016-06-14 00:48:43 +1000

    Create User Edit Profile Page
    Signed-off-by: wellsonjain <wellsonwei@ecoworkinc.com>

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
                   edit: 'profile'
                 },
                 controllers: {
-                  omniauth_callbacks: 'omniauth_callbacks'
+                  omniauth_callbacks: 'omniauth_callbacks',
+                  registrations: 'registrations'
                 }
 
   resources :users, only: [:show]
